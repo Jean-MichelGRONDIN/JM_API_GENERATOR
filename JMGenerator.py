4e0dcf414a7d697e7a91811c8b27e3650e991312
GRONDIN Jean-Michel <jean-michel.grondin@epitech.eu>
2021-11-20 14:34:15 +0400

    add like some files architecture to begin with

---
+++ JMGenerator.py
@@ -0,0 +1,5 @@
+from src.Generation.Generator import Generator
+
+if __name__ == "__main__":
+    generator = Generator()
+    generator.run()
---
+++ src/Generation/Generator.py
@@ -0,0 +1,6 @@
+class Generator:
+    def __init__(self):
+        print('Starting generator\n')
+
+    def run(self):
+        print('Running generator\n')
