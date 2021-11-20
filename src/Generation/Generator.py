d08889d49efc9c5c990606611f26eac5c88c503d
GRONDIN Jean-Michel <jean-michel.grondin@epitech.eu>
2021-11-20 20:24:13 +0400

    add comment on what to do later + save fist config file architecture

--- src/Generation/Generator.py
+++ src/Generation/Generator.py
@@ -4,3 +4,23 @@ class Generator:
 
     def run(self):
         print('Running generator\n')
+        # try:
+        #     #exec
+        # except:
+        #     #print error
+        #     #delete generated folder (dist/)
+
+
+# list les folders qu'il y à dans API/Routes/
+# puis entre dans cheque folder et retien le nom
+# puis lit chaque fichier et retien le nom du fichier
+# $NOM_DU_FICHIER + $NOM_DU_DOSSIER + $COMPLEMENT (Action / Data / Request / Sanitizer)
+# un fichier json par route
+# dans le fichier json donner : la method, sanitizer, request, middlewares
+# pas chercher si le model, le middleware, ... exist juste utilise les comme ci ils existait (ecrit les imports toi même)
+
+
+# list chaques fichiers dans table
+# le nom des fichiers c'est du style action_likes
+# génère les migration ? => Dire les fields, leur type (type model != type migration ? have to guess ?), si y'as des constraints, donner l'ordre dans lequel créer les migration (ptete repèrer des trucs avec les nom ? (has == liaison ?))
+# créer le model en transformant le nom du fichier en KamelCase singulier : ActionLike ( utiliser une regex ^([a-Z]) to upper et tous les _([a-Z]) to upper sans le _ )
